8e088ef44b229110dabf8076174d649611f208c0
Amazansia <asd111050@naver.com>
2022-12-26 23:57:18 +0900

    ktps - boj14402 done

--- src/main/boj_ps/boj14402.kt
+++ src/main/boj_ps/boj14402.kt
@@ -10,17 +10,12 @@ fun main() = with(System.`in`.bufferedReader()) {
 	for (i in 0 until q) {
 		var str = readLine().split(" ")
 		// 정상적인 출입일 경우 전체에서 뺌 A+, A-
-		if (str[1] == "+" && !hMap.containsKey(str[0])) {
-			hMap[str[0]] = 1
-		} else if (str[1] == "+" && hMap.containsKey(str[0])) {
-			hMap[str[0]] = hMap[str[0]]?.plus(1)
-		} else if (str[1] == "-" && hMap.containsKey(str[0])) {
-			hMap[str[0]]?.minus(1)
+		if (str[1] == "+") {
+			hMap[str[0]] = hMap.getOrDefault(str[0], 0)?.plus(1)
+		} else if (hMap.containsKey(str[0]) && hMap[str[0]] != 0) {
+			hMap[str[0]] = hMap[str[0]]?.minus(1)
 			sum += 2
-			if (hMap[str[0]] == 0) {
-				hMap.remove(str[0])
-			}
-
+//			println("$i, ${hMap[str[0]]}")
 		}
 	}
 
