3c57a2a13a486fff8f9ba8febb2348f72ae756b3
amazansia <asd111050@naver.com>
2023-01-06 23:23:38 +0900

    ktps - class2 solving

---
+++ src/main/boj_ps/boj10989.kt
@@ -0,0 +1,15 @@
+fun main() = with(System.`in`.bufferedReader()){
+    val N = readLine().toInt()
+
+    var list = IntArray(10001)
+    repeat(N){
+        list[readLine().toInt()]++
+    }
+
+    val bw = System.`out`.bufferedWriter()
+    for(i in list.indices){
+        bw.write("$i\n".repeat(list[i]))
+    }
+
+    bw.close()
+}
