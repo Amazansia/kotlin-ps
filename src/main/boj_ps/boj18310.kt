37d9e056f17aac3ada6fdbacded316727ded2948
amazansia <asd111050@naver.com>
2023-01-10 22:43:48 +0900

    ktps - resolve boj18310

--- src/main/boj_ps/boj18310.kt
+++ src/main/boj_ps/boj18310.kt
@@ -1,10 +1,6 @@
-import kotlin.math.*
-
 fun main() = with(System.`in`.bufferedReader()) {
-	val N = readLine().toInt()
-	val houses = readLine().split(" ").map { it.toInt() }.toIntArray()
-	var sumL: Long = 0
-	houses.forEach {
-		sumL += it
-	}
+    val N = readLine().toInt()
+    val houses = readLine().split(" ").map { it.toInt() }.toIntArray()
+    houses.sort()
+    println(houses[(houses.size - 1) / 2])
 }
