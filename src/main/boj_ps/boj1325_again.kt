5f2f51eb75ed9c6157f1d137655c598b2901160b
amazansia <asd111050@naver.com>
2023-01-13 22:44:33 +0900

    ktps - prepare for study20220114

---
+++ src/main/boj_ps/boj1325_again.kt
@@ -0,0 +1,50 @@
+import java.util.*
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val (N, M) = readLine().split(" ").map { it.toInt() }
+
+//    var nodes: Array<ArrayList<Int>> = Array(N + 1) { arrayListOf() }
+
+    var nodes = ArrayList<MutableList<Int>>()
+    repeat(N + 1) {
+        nodes.add(mutableListOf())
+    }
+
+    for (i in 0 until M) {
+        var (X, Y) = readLine().split(" ").map { it.toInt() }
+        nodes[Y].add(X)
+    }
+
+    // 정답 저장 리스트
+    var answerlist = IntArray(N + 1) { 0 }
+
+    // 가장 많은 컴퓨터 해킹: BFS - queue로 구현
+    // prim 알고리즘과 비슷하게 동작
+    for (i in 1..N) {
+        var q: Queue<Int> = LinkedList()
+        var visited = BooleanArray(N + 1)
+        q.add(i)
+        visited[i] = true
+
+        while (q.isNotEmpty()) {
+            var now = q.poll()
+            for (node in nodes[now]) {
+                if (node == 0) continue
+                if (!visited[node]) {
+                    answerlist[i]++
+                    q.add(node)
+                    visited[node] = true
+                }
+            }
+        }
+    }
+
+    // 리스트에서 정답 찾기
+    var maxValue = answerlist.maxOrNull()
+    var bw = System.`out`.bufferedWriter()
+    for (i in answerlist.indices) {
+        if (answerlist[i] == maxValue) bw.append("$i ")
+    }
+    bw.flush()
+    bw.close()
+}
---
+++ src/main/boj_ps/boj13549.kt
@@ -0,0 +1,44 @@
+import java.util.*
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val (N, K) = readLine().split(" ").map { it.toInt() }
+
+    val len = 100001
+    var visited = BooleanArray(len) { false }
+    var q: Queue<Pair<Int, Int>> = LinkedList()
+
+    visited[N] = true
+    q.add(Pair(N, 0))
+    var time = 0
+
+    while (q.isNotEmpty()) {
+        var front = q.poll()
+
+        if (front.first == K) {
+            time = front.second
+            break
+        }
+
+        if (front.first * 2 < len && !visited[front.first * 2]) {
+            q.add(Pair(front.first * 2, front.second))
+
+            visited[front.first * 2] = true
+        }
+
+        // 뒤로 걷기
+        if (front.first - 1 >= 0 && !visited[front.first - 1]) {
+            q.add(Pair(front.first - 1, front.second + 1))
+
+            visited[front.first - 1] = true
+        }
+        // 앞으로 걷기
+        if (front.first + 1 < len && !visited[front.first + 1]) {
+            q.add(Pair(front.first + 1, front.second + 1))
+
+            visited[front.first + 1] = true
+
+        }
+
+    }
+    println(time)
+}
