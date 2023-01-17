ff0a12efe92b6f3673a476c4e87d8574d4960d2e
amazansia <asd111050@naver.com>
2023-01-17 19:34:11 +0900

    ktps - class2 leveling...

---
+++ src/main/boj_ps/boj2751.kt
@@ -0,0 +1,14 @@
+import java.util.*
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val N = readLine().toInt()
+    var q = PriorityQueue<Int>()
+    for (i in 0 until N) {
+        q.add(readLine().toInt())
+    }
+    var sb = StringBuilder()
+    while (q.isNotEmpty()) {
+        sb.append("${q.poll()}\n")
+    }
+    print(sb)
+}
