9a79b2e3caaf6960daeeedc114f9ca9eddc91ee5
amazansia <asd111050@naver.com>
2023-01-22 20:19:15 +0900

    해피 설날

---
+++ src/main/boj_ps/boj11720.kt
@@ -0,0 +1,9 @@
+fun main() = with(System.`in`.bufferedReader()) {
+    readLine()
+    var arr = readLine()
+    var sum = 0
+    for (i in arr.indices) {
+        sum += arr[i].code - '0'.code
+    }
+    print(sum)
+}
