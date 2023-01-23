1d7671a59ca60a8c9a7e83ff4dc86587775d22bf
amazansia <asd111050@naver.com>
2023-01-23 23:09:37 +0900

    what

--- src/main/boj_ps/boj1343.kt
+++ src/main/boj_ps/boj1343.kt
@@ -1,24 +1,24 @@
 fun main() = with(System.`in`.bufferedReader()) {
-	var board = readLine()
-	var length = board.length
+    var board = readLine()
+    var length = board.length
 
-	var i = -1
+    var i = -1
 
-	while (++i < length) {
-		if (board[i] == '.') continue
-		if (i + 4 <= length && board.substring(i, i + 4) == "XXXX") {
-			board = board.replaceRange(i, i + 4, "AAAA")
-//			println("i = $i, board = $board")
+    while (++i < length) {
+        if (board[i] == '.') continue
+        if (i + 4 <= length && board.substring(i, i + 4) == "XXXX") {
+            board = board.replaceRange(i, i + 4, "AAAA")
 
-			i += 3
-			continue
-		} else if (i + 2 <= length && board.substring(i, i + 2) == "XX") {
-//			println("i = $i, board = $board")
-			board = board.replaceRange(i, i + 2, "BB")
-			i += 1
-			continue
-		}
-	}
 
-	print(if (board.contains("X")) -1 else board)
+            i += 3
+            continue
+        } else if (i + 2 <= length && board.substring(i, i + 2) == "XX") {
+
+            board = board.replaceRange(i, i + 2, "BB")
+            i += 1
+            continue
+        }
+    }
+
+    print(if (board.contains("X")) -1 else board)
 }
