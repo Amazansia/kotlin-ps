78282126f9b1adc572cd158c53a48744cc794f08
Amazansia <asd111050@naver.com>
2023-01-27 21:27:42 +0900

    ktps - prepare for 0128 study 3/N

---
+++ src/main/boj_ps/boj1253.kt
@@ -0,0 +1,30 @@
+fun main() = with(System.`in`.bufferedReader()) {
+	var N = readLine().toInt()
+	var arr = readLine().split(" ").map { it.toInt() }.toIntArray()
+	arr.sort()
+
+	var ans = 0
+
+	for (i in arr.indices) {
+		var left = 0
+		var right = N - 1
+		// left < right < N
+		while (left < right) {
+			if (right == N) break
+
+			if (arr[left] + arr[right] > arr[i]) {
+				right--
+			} else if (arr[left] + arr[right] < arr[i]) {
+				left++
+			} else if (left != i && right != i) {
+				ans++
+				break
+			}
+			if (left == i) left++
+			if (right == i) right--
+		}
+//		println(if (right == N) "yes" else "no")
+//		println("$left $right")
+	}
+	print(ans)
+}
