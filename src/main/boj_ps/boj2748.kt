99d3c0db547c96f36af574cdd56ddb1326799b34
Amazansia <asd111050@naver.com>
2023-02-02 17:47:14 +0900

    start dp...

---
+++ src/main/boj_ps/boj2748.kt
@@ -0,0 +1,11 @@
+fun main() = with(System.`in`.bufferedReader()) {
+	var N = readLine().toInt() // <= 90
+
+	var dp = LongArray(100)
+	dp[0] = 0
+	dp[1] = 1
+	for (i in 2 until 100) {
+		dp[i] = dp[i - 1] + dp[i - 2]
+	}
+	print(dp[N])
+}
