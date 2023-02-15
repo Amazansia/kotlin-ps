e1d60c9524cf4b5c0daa2e369d4d7ed52e39d785
Amazansia <asd111050@naver.com>
2023-02-15 20:12:46 +0900

    ktps - 이거s1아닌거같아진짜루

--- src/main/boj_ps/boj20364.kt
+++ src/main/boj_ps/boj20364.kt
@@ -1,3 +1,7 @@
 fun main() = with(System.`in`.bufferedReader()) {
-
+	var (N, Q) = readLine().split(" ").map { it.toInt() }
+	var landArray = IntArray(N)
+	repeat(Q) {
+		landArray[it] = readLine().toInt()
+	}
 }
