d36b5de3e9129c5db699d002a822d8306d8a38b8
amazansia <asd111050@naver.com>
2023-02-17 14:19:42 +0900

    no...

--- src/main/boj_ps/boj20364.kt
+++ src/main/boj_ps/boj20364.kt
@@ -1,12 +1,27 @@
 fun main() = with(System.`in`.bufferedReader()) {
-	var (N, Q) = readLine().split(" ").map { it.toInt() }
-	var duckNeedsArray = IntArray(N + 1)
-	repeat(Q) {
-		duckNeedsArray[it + 1] = readLine().toInt()
-	}
-	// 땅 번호 보니까 힙인 것 같아용... -> 완전이진트리, 배열로 저장
-	// 땅 ok -> 0, 안되면 처음 마주치는 점유된 땅 번호
-	// 점유 순서는 입력받은 순서대로
-	// 가는 경로상에 점유된 땅이 있다면... -> N이 2^20이므로 Int 내에서 ok,
-	// 저장 형식이 좀 헷갈리는...
+    // 땅 번호 보니까 힙인 것 같아용... -> 완전이진트리, 배열로 저장
+    // 땅 ok -> 0, 안되면 처음 마주치는 점유된 땅 번호
+    // 점유 순서는 입력받은 순서대로
+    // 가는 경로상에 점유된 땅이 있다면... -> N이 2^20이므로 Int 내에서 ok,
+    // 저장 형식이 좀 헷갈리는...
+
+    var (N, Q) = readLine().split(" ").map { it.toInt() }
+    var distint = HashSet<Int>()
+
+    var sb = StringBuilder()
+
+    for (i in 1..Q) {
+        var now = readLine().toInt()
+        var num = 0
+        var idx = now
+        while (idx >= 2) {
+            if (distint.contains(idx)) num = idx
+            idx /= 2
+        }
+
+        sb.append("$num\n")
+        if (num == 0) distint.add(now)
+    }
+
+    print(sb)
 }
--- src/main/boj_ps/test.kt
+++ src/main/boj_ps/test.kt
@@ -1,9 +1,10 @@
-import kotlin.time.*
+import kotlin.time.DurationUnit
+import kotlin.time.toDuration
 
 fun main() = with(System.`in`.bufferedReader()) {
-	var tv = TimedValue(Int, 10.toDuration(DurationUnit.MILLISECONDS))
+//	var tv = TimedValue(Int, 10.toDuration(DurationUnit.MILLISECONDS))
 
-	var test = 1397239847.toDuration(DurationUnit.MILLISECONDS)
-	println(test)
-	print(test.inWholeHours.toInt())
+    var test = 1397239847.toDuration(DurationUnit.MILLISECONDS)
+    println(test)
+    print(test.inWholeHours.toInt())
 }
