bd91f7bbd17a2d079112fb043e63986444394e6e
amazansia <asd111050@naver.com>
2023-03-21 22:33:35 +0900

    pgps - 이모티콘 할인 행사 vs 이모티콘 플러스

--- src/main/programmers_ps/kotlin/이모티콘할인행사/이모티콘할인행사.kt
+++ src/main/programmers_ps/kotlin/이모티콘할인행사/이모티콘할인행사.kt
@@ -1,2 +1,8 @@
 package kotlin.이모티콘할인행사
 
+class Solution {
+    fun solution(users: Array<IntArray>, emoticons: IntArray): IntArray {
+        var answer: IntArray = intArrayOf()
+        return answer
+    }
+}
