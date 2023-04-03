761e366921ad8b004e8931de356f91f64ba5592a
amazansia <asd111050@naver.com>
2023-04-03 11:34:36 +0900

    ktps - class4: boj2407

---
+++ src/main/boj_ps/boj2407.kt
@@ -0,0 +1,18 @@
+import java.math.BigInteger
+
+/*
+100 6
+
+* */
+fun main() = with(System.`in`.bufferedReader()) {
+    var (n, m) = readLine().trim().split(" ").map { it.toInt() }
+
+    var answer = BigInteger("1")
+    for (i in n - m + 1..n) {
+        answer = answer.multiply(BigInteger("$i"))
+    }
+    for (i in 1..m) {
+        answer = answer.divide(BigInteger("$i"))
+    }
+    print(answer)
+}
