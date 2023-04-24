e062b14e5268052a0dcbb412ec2d2d3ad9d2a3f9
amazansia <asd111050@naver.com>
2023-04-24 11:22:08 +0900

    ktps - boj15663

---
+++ src/main/boj_ps/boj15663.kt
@@ -0,0 +1,29 @@
+fun main() = with(System.`in`.bufferedReader()) {
+    var (N, M) = readLine().split(" ").map { it.toInt() }
+    var arr = readLine().split(" ").map { it.toInt() }.toIntArray()
+    arr.sort()
+
+    var visited = BooleanArray(arr.size)
+    var answerSet = mutableSetOf<List<Int>>()
+
+    // now: 인덱스
+    fun dfs(len: Int, combination: IntArray) {
+        if (len == M) {
+            answerSet.add(combination.toList())
+            return
+        }
+        for (i in arr.indices) {
+            if (visited[i]) continue
+            combination[len] = arr[i]
+            visited[i] = true
+            dfs(len + 1, combination)
+            visited[i] = false
+        }
+    }
+
+    dfs(0, IntArray(M))
+
+    answerSet.forEach {
+        println(it.joinToString(" "))
+    }
+}
