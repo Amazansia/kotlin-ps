be89b82b76ae10ddb3d2494c4bd1b486570a6aee
amazansia <asd111050@naver.com>
2023-04-29 13:32:40 +0900

    ktps - boj2178 complete

--- src/main/boj_ps/silver1/boj2178.kt
+++ src/main/boj_ps/silver1/boj2178.kt
@@ -1,40 +1,44 @@
 package silver1
 
+import java.util.*
+
 fun main() = with(System.`in`.bufferedReader()) {
     var (N, M) = readLine().split(" ").map { it.toInt() }
     var maze = Array(N) { IntArray(M) }
+    var count = Array(N) { IntArray(M) { 987654321 } }
     var visited = Array(N) { BooleanArray(M) }
+
 
     repeat(N) { i ->
         maze[i] = readLine().toCharArray().map { it.code - '0'.code }.toIntArray()
     }
 
-    var answer = Int.MAX_VALUE
+    var answer = 0
+    var now = 0 to 0
     var dxdy = arrayOf(1 to 0, 0 to 1, -1 to 0, 0 to -1)
 
-    fun dfs(count: Int, now: Pair<Int, Int>) {
-        visited[now.first][now.second] = true
-        if (now.first == N - 1 && now.second == M - 1) {
-            answer = kotlin.math.min(answer, count)
-            return
-        }
+    var pq =
+        PriorityQueue<Pair<Int, Int>>(compareBy<Pair<Int, Int>> { count[it.first][it.second] })
+//    PriorityQueue<Pair<Int, Int>>(compareBy<Pair<Int, Int>> { abs(now.first - it.first) + abs(now.second - it.second) })
 
+    pq.add(now)
+    visited[0][0] = true
+    count[0][0] = 1
+
+    while (pq.isNotEmpty()) {
+        now = pq.poll()
         for (i in 0 until 4) {
             var dx = now.first + dxdy[i].first
             var dy = now.second + dxdy[i].second
 
-            if (dx !in 0 until N || dy !in 0 until M || visited[dx][dy]) {
+            if (dx !in 0 until N || dy !in 0 until M || visited[dx][dy] || maze[dx][dy] == 0) {
                 continue
             }
-
-            if (maze[dx][dy] == 0)
-                continue
-
-            dfs(count + 1, dx to dy)
-            visited[dx][dy] = false
+            count[dx][dy] = kotlin.math.min(count[now.first][now.second] + 1, count[dx][dy])
+            visited[dx][dy] = true
+            pq.add(dx to dy)
         }
     }
 
-    dfs(1, 0 to 0)
-    println(answer)
+    println(count[N - 1][M - 1])
 }
