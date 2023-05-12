128e0455ed96e71d73dd78eaa2777b3700369433
amazansia <asd111050@naver.com>
2023-05-12 23:23:00 +0900

    pgps - 후보키 수정

--- src/main/programmers_ps/kotlin_/후보키/후보키.kt
+++ src/main/programmers_ps/kotlin_/후보키/후보키.kt
@@ -29,95 +29,63 @@ class Solution {
 
         // 속성에 대해 방문처리
         var visited = BooleanArray(relation[0].size)
-//		var parent = IntArray(relation[0].size) { -1 }
-//
-//		// 세미 유니온파인드
-//		// 가장 마지막에 나오는 속성의 index ->  parent[index] = t
-//		fun union(visited: BooleanArray) {
-//			for (i in visited.indices) {
-//				if(visited[i] && parent[i] == -1)
-//					parent[i] = t
-//			}
-//			t++
-//		}
 
-		fun checkCandidateKey(visited: BooleanArray): Boolean {
-			// 해당되는 속성만 뽑아서 keys에 저장
-			// [100, ryan] [200, apeach] [300, tube] [400, con] [500, muzi] [600, apeach]
-			var keys = relation.map { it.filterIndexed { index, _ -> visited[index] } }
-//			println(keys.joinToString(" "))
-//			 isUnique: 식별가능
-			// 이 함수가 특별하다:
-			// keys는 List<List<String>> 타입인데 이 내부의 List<String> 타입을 식별해 준다
-			// kotlin에서는 == 가 참조 타입의 값을 비교하여 동등성을 판별한다. 내부적으로는 equals를 호출함.
-			// 동등성: 두 개의 객체가 같은 정보를 갖고 있다
-			// In Java: eqauls()는 list 비교를 할 때, 각 리스트가 동일한 "원소"를 동일한 "순서"로 가지고 있다면 그것을 동일하다고 판단한다.
-			// 아하
-			// 그래서 it == key가 ([100, ryan] [200, apeach]) 기대했던 대로 true/false를 적절히 반환하는 듯
-			for (key in keys) {
-				if (keys.count { it == key } != 1) {
-//					println("cc")
-//					println(key)
-					return false
-				}
-			}
+        fun checkCandidateKey(visited: BooleanArray): Boolean {
+            // 해당되는 속성만 뽑아서 keys에 저장
+            // [100, ryan] [200, apeach] [300, tube] [400, con] [500, muzi] [600, apeach]
+            var keys = relation.map { it.filterIndexed { index, _ -> visited[index] } }
+            // isUnique: 식별가능
+            // 이 함수가 특별하다:
+            // keys는 List<List<String>> 타입인데 이 내부의 List<String> 타입을 식별해 준다
+            // kotlin에서는 == 가 참조 타입의 값을 비교하여 동등성을 판별한다. 내부적으로는 equals를 호출함.
+            // 동등성: 두 개의 객체가 같은 정보를 갖고 있다
+            // In Java: eqauls()는 list 비교를 할 때, 각 리스트가 동일한 "원소"를 동일한 "순서"로 가지고 있다면 그것을 동일하다고 판단한다.
+            // 그래서 it == key가 ([100, ryan] [200, apeach]) 기대했던 대로 true/false를 적절히 반환하는 듯
+            for (key in keys) {
+                if (keys.count { it == key } != 1) {
+                    return false
+                }
+            }
 
-//			// isDoublechecked
-//			for (i in visited.indices) {
-//				if (visited[i] && parent[i] != -1)
-//					return false
-//			}
+            // isMinimal
+            if (visited.count { it } == 1) {
+                return true
+            }
 
-			// isMinimal
-//			println(visited.joinToString(" "))
-//			println(visited.count { it })
-			if (visited.count { it } == 1) {
-//				println("isminimal???")
-				return true
-			}
+            var count = 0
+            for (i in 0 until keys[0].size) {
+                var testKeys = keys.map { it.filterIndexed { index, _ -> index != i } }
+                // 뺐으면 유일성이 유지되면 안 된다...
+                for (key in testKeys) {
+                    if (testKeys.count { it == key } != 1) {
+                        // 유일성 유지 실패 -> 즉 정답인 조합
+                        count++
+                        break
+                    }
+                }
+            }
 
-			var count = 0
-			for (i in 0 until keys[0].size) {
-				var testKeys = keys.map { it.filterIndexed { index, _ -> index != i } }
-				// 뺐으면 유일성이 유지되면 안 된다...
-				for (key in testKeys) {
-					if (testKeys.count { it == key } != 1) {
-						// 유일성 유지 실패 -> 즉 정답인 조합
-//						println(keys.joinToString(" "))
-//						union(visited)
-//						println(parent.joinToString(" "))
-						count++
-						break
-					}
-				}
-			}
+            return count == visited.count { it }
+        }
 
-//			println("here ffffffff$count")
+        fun dfs(visited: BooleanArray, now: Int) {
+            if (checkCandidateKey(visited)) {
+                answer++
+                return
+            }
 
-			return count == visited.count { it }
-		}
+            for (i in visited.indices) {
+                // 순서가 중요하지 않다 -> 1 3은 3 1과 같다. 즉, 중복될 수 있으니 순서대로 세어야 한다
+                if (!visited[i] && now <= i) {
+                    visited[i] = true
+                    dfs(visited, i)
+                    visited[i] = false
+                }
+            }
+        }
 
-		fun dfs(visited: BooleanArray, now: Int) {
-//			println(visited.joinToString(" "))
-			if (checkCandidateKey(visited)) {
-//				println("answer nextline")
-//				println(visited.joinToString(" "))
-				answer++
-				return
-			}
+        dfs(visited, 0)
 
-			for (i in visited.indices) {
-				// 순서가 중요하지 않다 -> 1 3은 3 1과 같다. 즉, 중복될 수 있으니 순서대로 세어야 한다
-				if (!visited[i] && now <= i) {
-					visited[i] = true
-					dfs(visited, i)
-					visited[i] = false
-				}
-			}
-		}
-
-		dfs(visited, 0)
-
-		return answer
-	}
+        return answer
+    }
 }
