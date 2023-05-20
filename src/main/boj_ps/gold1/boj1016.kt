8523158176bb1a3384a650eebd01bf3ad3474a01
amazansia <asd111050@naver.com>
2023-05-20 23:58:14 +0900

    ktps - boj1016

--- src/main/boj_ps/gold1/boj1016.kt
+++ src/main/boj_ps/gold1/boj1016.kt
@@ -1,5 +1,13 @@
 package gold1
 
+import kotlin.math.sqrt
+
 fun main() = with(System.`in`.bufferedReader()) {
     var (min, max) = readLine().split(" ").map { it.toLong() }
+    var size = (max - min + 1).toInt()
+    var list = MutableList<Long>(size) { -1 }
+    var sqrt = sqrt(max.toDouble()).toInt()
+    var k1 = MutableList(sqrt + 1) { -1 }
+    var k2 = mutableListOf<Long>()
+
 }
