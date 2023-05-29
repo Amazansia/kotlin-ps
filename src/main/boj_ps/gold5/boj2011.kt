d85645a8d96199974997522e922b9c50b1e2ef2c
amazansia <asd111050@naver.com>
2023-05-29 22:15:35 +0900

    ktps - boj2011 정답률 하락에 대박기여중...이런것도 기여로 쳐주라

--- src/main/boj_ps/gold5/boj2011.kt
+++ src/main/boj_ps/gold5/boj2011.kt
@@ -12,8 +12,29 @@ fun main() = with(System.`in`.bufferedReader()) {
     // answer[i] = password의 i번째 자리까지 고려했을 때 가질 수 있는 가장 큰 경우의 수
     var answer = IntArray(password.length)
 
+    if (password.isNotEmpty() && password[0] != '0') {
+        answer[0] = 1
+    } else {
+        println(0)
+        return@with
+    }
+
+    if (password.length >= 2 && password[1].isDigit()) {
+        answer[1] = 1
+        var num = password[0].digitToInt() * 10 + password[1].digitToInt()
+        if (num in 10..26) {
+            answer[1] = 2
+        } else if (num > 26 && num % 10 == 0) {
+            println(0)
+            return@with
+        }
+    } else {
+        println(0)
+        return@with
+    }
+
     var idx = 2
-    
+
     while (idx < password.length) {
         var countweird = 0
 
@@ -25,15 +46,21 @@ fun main() = with(System.`in`.bufferedReader()) {
 
         // 1자리 숫자 고려
         if (idx >= 1 && password[idx] != '0') {
-            answer[idx] = max(answer[idx], answer[idx] + answer[idx - 1])
-            answer[idx] %= 100000
+            answer[idx] = max(answer[idx - 1], answer[idx] + answer[idx - 1])
+            answer[idx] %= 1000000
         } else countweird++
 
         // 2자리 숫자 고려
         var num = password[idx - 1].digitToInt() * 10 + password[idx].digitToInt()
         if (idx >= 1 && num in 10..26) {
-            answer[idx] = max(answer[idx], answer[idx] + answer[idx - 2])
-            answer[idx] %= 100000
+            answer[idx] = max(answer[idx - 1], answer[idx] + answer[idx - 2])
+            answer[idx] %= 1000000
+            if (num == 10 || num == 20) {
+                answer[idx] = answer[idx - 2]
+            }
+        } else if (num > 26 && num % 10 == 0) {
+            println(0)
+            return@with
         } else countweird++
 
         if (countweird == 2) {
