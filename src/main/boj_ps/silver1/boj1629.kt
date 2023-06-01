c3f31b40dbb7c86dc442805559fd8fcab07470f6
amazansia <asd111050@naver.com>
2023-06-01 16:31:51 +0900

    ktps - boj1629

--- src/main/boj_ps/silver1/boj1629.kt
+++ src/main/boj_ps/silver1/boj1629.kt
@@ -1,9 +1,18 @@
 package silver1
 
 fun main() = with(System.`in`.bufferedReader()) {
-    var (A, B, C) = readLine().split(" ").map { it.toInt() }
+    var (A, B, C) = readLine().split(" ").map { it.toLong() }
 
-    var dp = IntArray(B)
+    var k: Long
 
+    fun recursion(num: Long): Long {
+        if (num == 0L) return 1
+        if (num == 1L) return A % C
 
+        k = recursion(num / 2) % C
+        if (num % 2 == 0L) return k * k % C
+        return k * k % C * A % C
+    }
+
+    println(recursion(B))
 }
