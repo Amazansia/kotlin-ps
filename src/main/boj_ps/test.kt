e684c70d6d628df933969464147ae26344dda9d5
amazansia <asd111050@naver.com>
2023-06-22 23:10:23 +0900

    코테정보삭제

--- src/main/boj_ps/test.kt
+++
@@ -1,9 +0,0 @@
-fun main() = with(System.`in`.bufferedReader()) {
-	var sb = StringBuilder()
-	repeat(100000) {
-		sb.append("1000")
-		sb.append("\n")
-	}
-	sb.deleteAt(sb.length - 1)
-	print(sb)
-}
