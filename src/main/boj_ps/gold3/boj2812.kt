bb90429e8f97528ad66eeed09d3051a9923143a5
amazansia <asd111050@naver.com>
2023-06-25 03:02:52 +0900

    스터디 준비

--- src/main/boj_ps/gold3/boj2812.kt
+++ src/main/boj_ps/gold3/boj2812.kt
@@ -1,5 +1,22 @@
 package gold3
 
 fun main() = with(System.`in`.bufferedReader()) {
+    var (_, K) = readLine().split(" ").map { it.toInt() }
+    var str = readLine()
+    var arr = str.toCharArray()
+    var dq = ArrayDeque<Char>()
+    for (i in arr.indices) {
+        while (K > 0 && dq.isNotEmpty() && dq.last() < arr[i]) {
+            dq.removeLast()
+            K--
+        }
+        dq.addLast(arr[i])
+    }
 
+    var sb = StringBuilder()
+    while (dq.size > K) {
+        sb.append(dq.removeFirst())
+    }
+
+    print(sb)
 }
--- src/main/boj_ps/gold5/boj5639.kt
+++ src/main/boj_ps/gold5/boj5639.kt
@@ -1,5 +1,24 @@
 package gold5
 
 fun main() = with(System.`in`.bufferedReader()) {
-    var N = readLine().toInt()
+    var tree = IntArray(10001)
+    var idx = 0
+    var now = readLine()
+    while (!now.isNullOrEmpty()) {
+        tree[idx++] = now.toInt()
+        now = readLine()
+        if (now.isNullOrEmpty()) break
+    }
+
+    fun recursive(n: Int, end: Int) {
+        if (n > end)
+            return
+        var mid = n + 1
+        while (mid <= end && tree[mid] < tree[n]) mid++
+        recursive(n + 1, mid - 1)
+        recursive(mid, end)
+        println(tree[n])
+    }
+
+    recursive(0, idx - 1)
 }
