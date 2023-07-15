a11eac0dab37296df4d2a445777dfa37cb4b360b
amazansia <asd111050@naver.com>
2023-07-16 01:41:38 +0900

    ktps - boj3649 어우징그러

--- src/main/boj_ps/gold5/boj3649.kt
+++ src/main/boj_ps/gold5/boj3649.kt
@@ -1,4 +1,9 @@
 package gold5
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import kotlin.system.exitProcess
+
 /*
 두 조각 길이의 합은 구멍의 너비와 정확하게 일치해야 한다...
 구멍을 완벽하게 막을 수 있는 두 조각은?
@@ -12,38 +17,42 @@ n 백만개들어옴
 될듯
 정렬함하고
 s / e 쓰면서
-
 */
 
-fun main() = with(System.`in`.bufferedReader()) {
+fun main() {
+    try {
+        val br = BufferedReader(InputStreamReader(System.`in`))
+        solve@ while (true) {
 
+            var X = br.readLine().toInt() * 10000000
+            var N = br.readLine().toInt()
 
-    fun solve(str: String) {
-        //  200000000
-        var X = str.toInt() * 10000000
-        var N = readLine().toInt()
-        var arr = BooleanArray(X)
+            var arr = IntArray(N)
 
-        for (i in 0 until N) {
-            var num = readLine().toInt()
-            if (num >= X) continue
-            arr[num] = true
-        }
-
-        for (i in 0..X / 2) {
-            if (arr[i] && arr[X - i]) {
-                println("yes $i ${X - i}")
-                return
+            for (i in 0 until N) {
+                arr[i] = br.readLine().toInt()
             }
+
+            if (N == 0 || X < 0) {
+                println("danger")
+                continue@solve
+            }
+
+            arr.sort()
+
+            var l = 0
+            var r = N - 1
+
+            while (l < r) {
+                var sum = arr[l] + arr[r]
+                if (sum == X) break
+                if (sum < X) l++
+                if (sum > X) r--
+            }
+
+            println(if (l >= r) "danger" else "yes ${arr[l]} ${arr[r]}")
         }
-        println("danger")
+    } catch (e: Exception) {
+        exitProcess(0)
     }
-
-    var str = readLine()
-
-    while (!str.isNullOrEmpty()) {
-        solve(str)
-        str = readLine()
-    }
-
 }
