073aea11f7d8e56686d41ece33e9ed3a39554153
amazansia <asd111050@naver.com>
2023-07-26 19:30:18 +0900

    ktps - boj11053

--- src/main/boj_ps/silver2/boj11053.kt
+++ src/main/boj_ps/silver2/boj11053.kt
@@ -6,24 +6,39 @@ fun main() = with(System.`in`.bufferedReader()) {
     var N = readLine().toInt()
     var arr = readLine().split(" ").map { it.toInt() }.toIntArray()
 
-    var list = IntArray(N)
-    list[0] = arr[0]
+    var list = mutableListOf<Int>()
+    list.add(arr[0])
 
-    var idx = 0
-    // arr[i]가 들어갈 수 있는 최적의 위치를 찾아봅시다
-    // 10 20 10 30 20 50
-    // 10 20 30 50
-    // 이때 50의 자리는
-    for (i in 1 until N) {
-        // list에 저장된 현재 원소보다 arr[i]가 작다면 arr[i]로 갱신해야 함
-        if (list[idx] > arr[i]) {
-            list[idx] = arr[i]
+    // 1 6 4 5 2 3 5 7 9 8
+    /*
+    1 6
+    1 4
+    1 4 5
+
+    * */
+
+    // 자리 찾아주는 이분탐색 함수
+    // s~e 사이에서 num이 위치해야 하는 idx값을 리턴한다
+    fun bs(s: Int, e: Int, num: Int): Int {
+        var start = s
+        var end = e
+        while (start < end) {
+            var mid = (start + end) / 2
+            if (num > list[mid]) start = mid + 1
+            else end = mid
         }
-        // list에 저장된 현재 원소보다 arr[i]가 크다면 덧붙여줍시다
-        if (list[idx] < arr[i]) {
-            list[++idx] = arr[i]
-        }
+        return end
     }
 
-    println(list.count { it != 0 })
+    for (i in 1 until N) {
+        // list.last보다 arr[i]가 크다면 arr[i]로 덧붙여준다
+        if (list.last() < arr[i]) {
+            list.add(arr[i])
+        }
+        // last보다 arr[i]가 작다면 arr[i]의 위치를 찾아줘야 함
+        var idx = bs(0, list.size - 1, arr[i])
+        list[idx] = arr[i]
+    }
+
+    println(list.size)
 }
