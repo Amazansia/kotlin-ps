cbe598e0c5a3c29c285469c47cbb32d56c94bd46
amazansia <asd111050@naver.com>
2023-08-14 21:48:56 +0900

    ktps - boj2448

--- src/main/boj_ps/gold4/boj2448.kt
+++ src/main/boj_ps/gold4/boj2448.kt
@@ -1,4 +1,11 @@
 package gold4
+/*
+재귀
+3줄짜리 125삼각형을 그리는 함수 smallTriangle()
+3 * 2^10 -> 2048 * 3 = 6천얼마
+가장 밑줄에 깔리는 125삼각형의 개수는 N/3개
+
+* */
 
 fun main() = with(System.`in`.bufferedReader()) {
     var N = readLine().toInt()
