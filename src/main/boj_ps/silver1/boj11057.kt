ebb7e9c3ea4ac34680e13b1d8b3d77d251fa73be
Amazansia <asd111050@naver.com>
2023-08-28 13:36:22 +0900

    ktps - boj11057

---
+++ src/main/boj_ps/silver1/boj11057.kt
@@ -0,0 +1,36 @@
+package silver1
+/*
+오름차순 수
+오르막수의 개수를 10007로 나눈 나머지 출력
+수는 0으로 시작할 수 있다
+길이에 따라 동일한 수가 반복되어야 한다
+마지막 수에 따라 다음에 올 수 있는 수의 개수가 달라진다
+dp[i][j]: i의 길이를 가진 수면서 j로 끝나는 수의 개수
+dp[1][0~9] = 1
+ex) dp[2][3] = dp[1][0] + dp[1][1] + dp[1][2] + dp[1][3]
+MOD 조심...
+* */
+
+fun main() = with(System.`in`.bufferedReader()) {
+	var N = readln().toInt()
+	val MOD = 10007
+	var dp = Array(N + 1) { IntArray(10) }
+
+	for (i in 0 until 10) {
+		dp[1][i] = 1
+	}
+
+	for (i in 2..N) {
+		for (j in 0..9) {
+			for (k in 0..j) {
+				dp[i][j] = (dp[i][j] + dp[i - 1][k]) % MOD
+			}
+		}
+	}
+
+	var answer = 0
+	for(i in 0 .. 9){
+		answer = (answer + dp[N][i]) % MOD
+	}
+	println(answer)
+}
