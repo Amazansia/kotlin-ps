24b2b5aeed920f7f347dfadf65a5e6a6544243dc
amazansia <asd111050@naver.com>
2023-08-31 12:59:30 +0900

    ktps - boj19236...

--- src/main/boj_ps/gold2/boj19236.kt
+++ src/main/boj_ps/gold2/boj19236.kt
@@ -1,4 +1,12 @@
 package gold2
 
 fun main() = with(System.`in`.bufferedReader()) {
+    var info = Array(4) { Array(4) { IntArray(2) } }
+
+    for (i in 0 until 4) {
+        var str = readln().split(" ").map { it.toInt() }
+        for (j in 0 until 4) {
+            info[i][j] = intArrayOf(str[j * 2], str[j * 2 + 1])
+        }
+    }
 }
