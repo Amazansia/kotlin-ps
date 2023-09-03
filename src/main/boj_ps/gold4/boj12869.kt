0973a0ee437fc48d476e870fe65a3f5e7575e6b2
amazansia <asd111050@naver.com>
2023-09-03 23:49:44 +0900

    ktps - boj12869.........

--- src/main/boj_ps/gold4/boj12869.kt
+++ src/main/boj_ps/gold4/boj12869.kt
@@ -1,7 +1,5 @@
 package gold4
 
-import java.util.*
-
 /*
 첫딜 9 두번째 3 세번째 1
 체력 0 이하일 때 scv 파괴
@@ -21,7 +19,7 @@ N == 3이면...
 dfs 써야함
 순서조합 6개 다 돌려봐야됨
 즉 시복 6^N
-
+N==2일때...즉 arr 마지막 원소를 0으로 놓고 걍 돌리면 되는거네 ㅠ
 
 * */
 
@@ -29,40 +27,31 @@ fun main() = with(System.`in`.bufferedReader()) {
     var N = readln().toInt()
     var arr = readLine().split(" ").map { it.toInt() }.toIntArray()
 
+    var answer = Int.MAX_VALUE
 
-    when (N) {
-        1 -> {
-            println(arr[0] / 9 + if (arr[0] % 9 == 0) 0 else 1)
+    fun dfs(n1: Int, n2: Int, n3: Int, cnt: Int) {
+
+        if (cnt > answer) return
+        if (n1 <= 0 && n2 <= 0 && n3 <= 0) {
+            answer = kotlin.math.min(answer, cnt)
+            return
         }
 
-        2 -> {
-            var pq = PriorityQueue<Int>(Comparator.reverseOrder())
-            pq.add(arr[0])
-            pq.add(arr[1])
-
-            var answer = 0
-
-            while (pq.isNotEmpty()) {
-
-                answer++
-
-                var first = pq.poll()
-                var second = 0
-                if (pq.isNotEmpty()) second = pq.poll()
-                if (first - 9 > 0) pq.add(first - 9)
-                if (second - 3 > 0) pq.add(second - 3)
-            }
-
-            println(answer)
+        if (n1 > 0) {
+            dfs(n1 - 9, n2 - 3, n3 - 1, cnt + 1)
+            dfs(n1 - 9, n2 - 1, n3 - 3, cnt + 1)
         }
-
-        else -> {
-            var answer = 0
-
-
-            // 6개 경우의 수 다 체크하면서 돌려야됨
-            dfs()
+        if (n2 > 0) {
+            dfs(n1 - 3, n2 - 9, n3 - 1, cnt + 1)
+            dfs(n1 - 1, n2 - 9, n3 - 3, cnt + 1)
+        }
+        if (n3 > 0) {
+            dfs(n1 - 3, n2 - 1, n3 - 9, cnt + 1)
+            dfs(n1 - 1, n2 - 3, n3 - 9, cnt + 1)
         }
     }
+
+    dfs(arr[0], if (arr.size == 2) arr[1] else 0, if (arr.size == 3) arr[2] else 0, 0)
+    println(answer)
 
 }
