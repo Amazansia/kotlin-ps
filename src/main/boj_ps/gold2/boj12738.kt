7e8d411afb6ba26739cdec79213694da337e330c
amazansia <asd111050@naver.com>
2023-09-18 22:04:56 +0900

    ktps - boj12738

--- src/main/boj_ps/gold2/boj12738.kt
+++ src/main/boj_ps/gold2/boj12738.kt
@@ -9,5 +9,36 @@ NlogN...이면
 * */
 
 fun main() = with(System.`in`.bufferedReader()) {
+    var N = readLine().toInt()
+    var arr = readLine().split(" ").map { it.toInt() }.toIntArray()
 
+    var LIS = IntArray(N)
+
+    // LIS의 시작 인덱스와 끝 인덱스, num을 받아 LIS 내부에서 num이 위치해야 하는 idx값을 리턴하는 함수
+    fun binarySearch(start: Int, end: Int, num: Int): Int {
+        var s = start
+        var e = end
+
+        while (s < e) {
+            var mid = (s + e) / 2
+            if (LIS[mid] < num) {
+                s = mid + 1
+            } else
+                e = mid
+        }
+
+        return e
+    }
+
+    var idx = 0
+    LIS[0] = arr[0]
+
+    for (i in arr.indices) {
+        if (LIS[idx] < arr[i])
+            LIS[++idx] = arr[i]
+        else
+            LIS[binarySearch(0, idx, arr[i])] = arr[i]
+    }
+
+    println(idx + 1)
 }
